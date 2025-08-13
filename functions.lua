a85167496939c8c775b5b0136a9db97b5111da50
Vít Starý Novotný <witiko@mail.muni.cz>
2025-08-13 14:14:01 +0200

    Pass pathname and options to `new_issues()`

--- functions.lua
+++ functions.lua
@@ -17,12 +17,12 @@ local files = assert(io.open("files.txt", "r"))
 local function_seen_names, function_seen_pathnames = {}, {}
 local function_names, function_pathnames = {}, {}
 for pathname in files:lines() do
-  local issues = new_issues()
   local file = assert(io.open(pathname, "r"))
   local content = assert(file:read("*a"))
   assert(file:close())
-  local results = {}
   local options = {fail_fast = false}
+  local issues = new_issues(pathname, options)
+  local results = {}
   utils.process_with_all_steps(pathname, content, issues, results, options)
 
   -- Extract the function definitions from the analysis results.
